5d8da85bed5411501974b83a5a4453298f5ef127
Norbert <nobertdalton@gmail.com>
2022-09-03 15:38:14 -0400

    fist update

---
+++ src/main/java/com/helloword/Controller/helloController.java
@@ -0,0 +1,15 @@
+package com.helloword.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class helloController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String handle1(){
+        return  "hello word";
+    }
+}
